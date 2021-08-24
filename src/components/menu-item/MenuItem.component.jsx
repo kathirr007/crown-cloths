c205242bfbae4fd08065bf9526bd9b07eae653db
Kathiravan Kumarasamy <kathiravan.kumarasamy@adelaide.edu.au>
2021-08-24 14:05:04 +0530

    refactor homepage component into smaller components and updated styles

---
+++ src/components/menu-item/MenuItem.component.jsx
@@ -0,0 +1,18 @@
+import "./MenuItem.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`menu-item ${size}`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <div className="title">{title}</div>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
