d1563dec7d74bc40a257a21d008042ab8588c45b
Kathiravan Kumarasamy <kathiravan.kumarasamy@adelaide.edu.au>
2021-08-24 21:53:27 +0530

    Add react-router-dom and Update menu item to navigate to detail page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/homepage/HomePage.component";
+
+const HatsPage = () => {
+  <div>
+    <h1>HATS Page</h1>
+  </div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/MenuItem.component.jsx
+++ src/components/menu-item/MenuItem.component.jsx
@@ -1,7 +1,17 @@
+import { withRouter } from "react-router-dom";
 import "./MenuItem.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`menu-item ${size}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    onKeyUp={(e) =>
+      (e.key === "Enter" || e.key === "NumpadEnter") &&
+      history.push(`${match.url}${linkUrl}`)
+    }
+    role="navigation"
+    tabIndex="0"
+  >
     <div
       className="background-image"
       style={{
@@ -11,8 +21,9 @@ const MenuItem = ({ title, imageUrl, size }) => (
     <div className="content">
       <div className="title">{title}</div>
       <span className="subtitle">SHOP NOW</span>
+      <img src alt="" />
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
