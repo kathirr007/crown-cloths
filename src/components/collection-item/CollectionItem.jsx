3581e0fd22c0d0f88b488668c54176daf7145385
Kathiravan Kumarasamy <kathiravan.kumarasamy@adelaide.edu.au>
2021-08-28 13:14:44 +0530

    Updated shop data with new data, ad cart utils to group items in cart reducer

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,18 +1,33 @@
+import { connect } from 'react-redux'
+import { addItem } from '@/redux/cart/cart.actions'
+
+import CustomButton from '@/components/custom-button/CustomButton'
+
 import './CollectionItem.scss'
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='collection-footer'>
-      <div className='name'>{name}</div>
-      <div className='price'>{price}</div>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='collection-footer'>
+        <div className='name'>{name}</div>
+        <div className='price'>{price}</div>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add to Cart
+      </CustomButton>
     </div>
-  </div>
-)
+  )
+}
 
-export default CollectionItem
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -1,8 +1,15 @@
 import './CustomButton.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  inverted,
+  isGoogleSignIn,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
