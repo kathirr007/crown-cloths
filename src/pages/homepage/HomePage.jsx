d313284d26e8a18964e6664bc0164c92e757330e
Kathiravan Kumarasamy <kathiravan.kumarasamy@adelaide.edu.au>
2021-09-01 22:21:49 +0530

    Add styled-components to refactor styles

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Directory from '@/components/directory/Directory'
 
-import './HomePage.scss'
+// import './HomePage.scss'
+
+import { HomePageContainer } from './HomePage.styles.jsx'
 
 const HomePage = () => (
-  <div className='homepage'>
+  <HomePageContainer>
     <h1>Welcome to my Homepage</h1>
     <Directory />
-  </div>
+  </HomePageContainer>
 )
 
 export default HomePage
