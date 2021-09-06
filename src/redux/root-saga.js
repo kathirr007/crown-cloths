0ccc7540921c6351d6e25e693f0e76c3c5d30794
Kathiravan Kumarasamy <kathiravan.kumarasamy@adelaide.edu.au>
2021-09-06 19:51:44 +0530

    Add roo-saga to avoid multiple sagas in store file

---
+++ src/redux/root-saga.js
@@ -0,0 +1,7 @@
+import { all, call } from '@redux-saga/core/effects'
+
+import { fetchCollectionsStart } from './shop/shop.sagas'
+
+export default function* rootSaga() {
+  yield all([call(fetchCollectionsStart)])
+}
